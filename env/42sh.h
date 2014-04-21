b893e74e3b5fd9b0fc1c88e54508cf14c9dbc51b
garcia_t <garcia_t@epitech.eu>
2014-04-21 14:23:06 +0200

    commit garcia

---
+++ env/42sh.h
@@ -0,0 +1,28 @@
+/*
+** 42sh.h for 42sh in /home/garcia_t/rendu/42sh
+** 
+** Made by garcia antoine
+** Login   <garcia_t@epitech.net>
+** 
+** Started on  Fri Apr  4 12:16:42 2014 garcia antoine
+** Last update Sat Apr 19 15:13:41 2014 garcia antoine
+*/
+
+#ifndef SHELL_H_
+# define SHELL_H_
+# include "list.h"
+
+typedef struct s_42sh
+{
+  t_dlist	*env;
+  char		*cmd;
+}		t_42sh;
+
+int	isolate_name_and_value_from_env(char *env);
+char	*get_name_from_env(char *env);
+char	*get_value_from_env(char *env);
+char	*get_env(char *name, t_dlist *env);
+char	**list_to_tab(t_dlist *env);
+int	start_shell(t_42sh *shell);
+
+#endif /*SHELL_H_ */
---
+++ env/main.c
@@ -0,0 +1,44 @@
+/*
+** main.c for main in /home/garcia_t/rendu/42sh
+** 
+** Made by garcia antoine
+** Login   <garcia_t@epitech.net>
+** 
+** Started on  Fri Apr  4 12:20:14 2014 garcia antoine
+** Last update Sat Apr 19 16:22:08 2014 garcia antoine
+*/
+
+#include <stdlib.h>
+#include "42sh.h"
+#include "lexer.h"
+
+void	free_tab(char **tab)
+{
+  int	i;
+
+  i = 0;
+  while (tab[i] != '\0')
+    {
+      free(tab[i]);
+      i++;
+    }
+  free(tab);
+}
+
+void	free_struct_shell(t_42sh *shell)
+{
+  if (shell->env->lenght > 0)
+    free_my_list(shell->env);
+  if (shell->cmd != NULL)
+    free(shell->cmd);
+}
+
+int	main(int ac, char **av, char **env)
+{
+  t_42sh	shell;
+
+  init_my_shell(&shell, env);
+  start_shell(&shell);
+  free_struct_shell(&shell);
+  return (0);
+}
