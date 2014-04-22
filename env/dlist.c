265af458b4aa007027f71ab351cd593e1f42efef
charvo_a <nicolas.charvoz@epitech.eu>
2014-04-22 14:11:14 +0200

    commit charvoz

--- env/dlist.c
+++ env/dlist.c
@@ -5,7 +5,7 @@
 ** Login   <garcia_t@epitech.net>
 **
 ** Started on  Fri Apr  4 11:55:23 2014 garcia antoine
-** Last update Tue Apr 22 13:54:34 2014 Nicolas Charvoz
+** Last update Tue Apr 22 14:09:36 2014 Nicolas Charvoz
 */
 
 #include <stdlib.h>
