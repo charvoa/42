139be7447f507ebabfb60916713ead8ec15be566
garcia_t <garcia_t@epitech.eu>
2014-05-08 16:50:39 +0200

    garcia

---
+++ execution/global_exec.c
@@ -0,0 +1,57 @@
+/*
+** global_exec.c for global_exec in /home/garcia_t/42/execution
+** 
+** Made by garcia antoine
+** Login   <garcia_t@epitech.net>
+** 
+** Started on  Thu May  8 16:21:37 2014 garcia antoine
+** Last update Thu May  8 16:50:08 2014 garcia antoine
+*/
+
+#include <unistd.h>
+#include "../parser/parser.h"
+
+void	exec_cmd(t_cmd *cmd, t_42sh *shell)
+{
+  int	i;
+  char	*path;
+
+  i = 0;
+  while(shell->path[i])
+    {
+      path = strcat(shell->path[i], cmd->args[0]);
+      execve(path, cmd->args, shell->envtab);
+      i++;
+    }
+}
+
+void	check_cmd(t_cmd *cmd, t_42sh *shell)
+{
+  int	check;
+
+  check =  access(cmd->args[0], X_OK);
+}
+
+int	exec_cmd_simple(t_cmd *cmd, t_42sh *shell)
+{
+  int	pid;
+  int	status;
+
+  pid = fork();
+  if (pid == -1)
+    return (0);
+  if (pid == 0)
+    {
+      check_cmd(cmd, shell);
+      exec_cmd(cmd, shell);
+      exit(1);
+    }
+  else
+    wait(&status);
+}
+
+void	global_exec(t_cmd *cmd, t_42sh *shell)
+{
+ if (cmd->token == NULL || strcmp(cmd->token, ";") == 0)
+   exec_cmd_simple(cmd, shell);
+}
