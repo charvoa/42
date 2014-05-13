f25d273bdd3f63c38acb99bab384d87efa57fa20
charvo_a <nicolas.charvoz@epitech.eu>
2014-05-13 14:36:18 +0200

    charvoz

--- env/prompt.c
+++ env/prompt.c
@@ -5,7 +5,7 @@
 ** Login   <garcia_t@epitech.net>
 **
 ** Started on  Fri Apr  4 15:59:36 2014 garcia antoine
-** Last update Tue May  6 13:57:20 2014 Nicolas Charvoz
+** Last update Tue May 13 14:28:31 2014 Nicolas Charvoz
 */
 
 #include <stdlib.h>
