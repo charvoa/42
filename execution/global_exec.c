c59b120f6e2be93a02bf453db16bcc9b31c4fadf
LunArctic <nicolas.girardot@gmail.com>
2014-05-14 14:33:44 +0200

    lol

--- execution/global_exec.c
+++ execution/global_exec.c
@@ -1,20 +1,11 @@
 /*
 ** global_exec.c for global_exec in /home/garcia_t/42/execution
-<<<<<<< HEAD
-**
+
 ** Made by garcia antoine
 ** Login   <garcia_t@epitech.net>
 **
 ** Started on  Thu May  8 16:21:37 2014 garcia antoine
-** Last update Tue May 13 16:07:58 2014 Nicolas Girardot
-=======
-** 
-** Made by garcia antoine
-** Login   <garcia_t@epitech.net>
-** 
-** Started on  Thu May  8 16:21:37 2014 garcia antoine
-** Last update Tue May 13 15:18:43 2014 garcia antoine
->>>>>>> 0356f97866e3acb62f856c59814eae75d5ac3ce8
+** Last update Wed May 14 14:32:41 2014 Nicolas Girardot
 */
 
 #include <unistd.h>
@@ -37,14 +28,11 @@ int	global_exec(t_cmd *cmd, t_42sh *shell, int tok)
 	  redirections(&cmd[i], &cmd[i + 1], shell);
 	  break;
 	}
-<<<<<<< HEAD
       else if (!strcmp(cmd[i].token, "|"))
 	{
 	  init_pipes(&cmd[i], &cmd[i + 1], shell);
 
 	}
-=======
->>>>>>> 0356f97866e3acb62f856c59814eae75d5ac3ce8
       i++;
       tok--;
     }
