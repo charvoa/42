38a5b6fbe9552ec514ad9a6938ae810fa3a76893
Serge <serge.heitzler@epitech.eu>
2014-05-17 16:38:33 +0200

    modif serge grosse pipe

--- pipe/pipes.c
+++ pipe/pipes.c
@@ -5,7 +5,7 @@
 ** Login   <heitzl_s@epitech.net>
 **
 ** Started on  Wed May 14 15:05:12 2014 heitzl_s
-** Last update Thu May 15 11:06:59 2014 heitzl_s
+** Last update Sat May 17 16:37:15 2014 heitzl_s
 */
 
 #include <unistd.h>
@@ -15,34 +15,81 @@
 #include "../env/42sh.h"
 #include "pipe.h"
 
-int             init_pipes(t_cmd *cmd_1, t_cmd *cmd_2, t_42sh *shell)
+int    check_pipe_cmd(t_cmd *cmd, t_42sh *shell)
 {
-  pid_t         pid;
-  pid_t         pid2;
+  int   i;
+  char  *path;
+
+  i = 0;
+  if (check_cmd(cmd, shell) == 0)
+    {
+      if (shell->path == NULL)
+	{
+	  printf("NEW Command not found\n");
+	  return (0);
+	}
+      while(shell->path[i])
+	{
+	  path = strcat(shell->path[i], cmd->args[0]);
+	  if (access(path, X_OK) == 0)
+	    return (0);
+	  i++;
+	}
+    }
+  return (-1);
+}
+
+int	exec_first_cmd(t_cmd *cmd, int *fd, t_42sh *shell)
+{
+  int	close_fd;
+  pid_t	pid;
+
+  close_fd = fd[0];
+  pid = fork();
+  if (pid == 0)
+    {
+      dup2(fd[1], 1);
+      dup2(0, 0);
+      close(close_fd);
+      if (exec_cmd(cmd, shell) == -1)
+	printf("Command not found : %s\n", cmd->args[0]);
+    }
+  return (pid);
+}
+
+int	exec_second_cmd(t_cmd *cmd, int *fd, t_42sh *shell)
+{
+  int	close_fd;
+  pid_t	pid;
+
+  close_fd = fd[1];
+  pid = fork();
+  if (pid == 0)
+    {
+      dup2(1, 1);
+      dup2(fd[0], 0);
+      close(close_fd);
+      if (exec_cmd(cmd, shell) == -1)
+	printf("Command not found : %s\n", cmd->args[0]);
+    }
+  return (pid);
+}
+
+int		init_pipes(t_cmd *cmd_1, t_cmd *cmd_2, t_42sh *shell, int i)
+{
+  pid_t		pid_1;
+  pid_t		pid_2;
   int           fd[2];
   int           status;
 
   pipe(fd);
   cmd_2->type = 1;
-  test(cmd_1, 0, fd[1], shell, fd[0]);
-  test(cmd_2, fd[0], 1, shell, fd[1]);
+  pid_1 = exec_first_cmd(cmd_1, fd, shell);
+  if (pid_1 >= 0)
+    pid_2 = exec_second_cmd(cmd_2, fd, shell);
   close(fd[0]);
   close(fd[1]);
-  wait(&status);
-  wait(&status);
-}
-
-int	test(t_cmd *cmd, int fdi, int fdo, t_42sh *shell, int close_fd)
-{
-  pid_t	pid;
-
-  pid = fork();
-  if (pid == 0)
-    {
-      dup2(fdo, 1);
-      dup2(fdi, 0);
-      close(close_fd);
-      exec_cmd(cmd, shell);
-    }
-  return (pid);
+  waitpid(pid_1, &status, 0);
+  waitpid(pid_2, &status, 0);
+  return (0);
 }
