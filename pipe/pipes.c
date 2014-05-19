46559ab8aee64bae5032072bf7b351301c280633
LunArctic <nicolas.girardot@gmail.com>
2014-05-19 16:43:41 +0200

    new pipes

--- pipe/pipes.c
+++ pipe/pipes.c
@@ -5,7 +5,7 @@
 ** Login   <heitzl_s@epitech.net>
 **
 ** Started on  Wed May 14 15:05:12 2014 heitzl_s
-** Last update Mon May 19 10:58:25 2014 Nicolas Girardot
+** Last update Mon May 19 16:39:34 2014 Nicolas Girardot
 */
 
 #include <unistd.h>
@@ -23,19 +23,14 @@ int    check_pipe_cmd(t_cmd *cmd, t_42sh *shell)
   i = 0;
   if (access(cmd->args[0], X_OK) == - 1)
     {
-      while(shell->path[i])
-	{
-	  path = strcat(shell->path[i], cmd->args[0]);
-	  if (access(path, X_OK) == 0)
-	    return (0);
-	  i++;
-	}
-      return (-1);
+      path = real_path(cmd, shell);
+      if (path != NULL)
+	return (0);
+      else
+	return (-1);
     }
   else
-    {
-      return (0);
-    }
+    return (0);
 }
 
 int	exec_first_cmd(t_cmd *cmd, int *fd, t_42sh *shell)
@@ -55,7 +50,8 @@ int	exec_first_cmd(t_cmd *cmd, int *fd, t_42sh *shell)
       dup2(fd[1], 1);
       dup2(0, 0);
       close(close_fd);
-      exec_cmd(cmd, shell);
+      if (exec_cmd(cmd, shell) == -1)
+	printf("Command not found : %s\n", cmd->args[0]);
       exit (pid);
     }
   return (pid);
@@ -92,9 +88,8 @@ int		init_pipes(t_cmd *cmd_1, t_cmd *cmd_2, t_42sh *shell, int i)
   int           fd[2];
   int           status;
 
-  pid_2 = -1;
   pipe(fd);
-  cmd_2->type = 1;
+  cmd_2->type = 2;
   pid_1 = exec_first_cmd(cmd_1, fd, shell);
   if (pid_1 >= 0)
     pid_2 = exec_second_cmd(cmd_2, fd, shell);
