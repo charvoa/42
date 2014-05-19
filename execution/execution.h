2ace1d00821092bd14002d7cc146d0d58f046041
garcia_t <garcia_t@epitech.eu>
2014-05-19 11:42:05 +0200

    garcia

--- execution/execution.h
+++ execution/execution.h
@@ -5,11 +5,17 @@
 ** Login   <garcia_t@epitech.net>
 **
 ** Started on  Fri May  9 11:18:26 2014 garcia antoine
-** Last update Thu May 15 15:49:42 2014 garcia antoine
+** Last update Mon May 19 11:34:24 2014 garcia antoine
 */
 
 #ifndef EXECUTION_H_
 # define EXECUTION_H_
+
+typedef struct s_signal
+{
+  int	nb;
+  char	*msg;
+}		t_signal;
 
 /* redirections.c */
 int	redir_left(t_cmd*, t_cmd*, t_42sh*);
