2258bdc4d581910923726a559a0403778c70a123
LunArctic <nicolas.girardot@gmail.com>
2014-05-22 12:50:01 +0200

    xpipe

--- xlib/xpipe.c
+++ xlib/xpipe.c
@@ -5,14 +5,14 @@
 ** Login   <charvo_a@epitech.net>
 **
 ** Started on  Wed May 21 14:20:51 2014 Nicolas Charvoz
-** Last update Wed May 21 14:29:29 2014 Nicolas Charvoz
+** Last update Thu May 22 12:49:36 2014 Nicolas Girardot
 */
 
 #include "xlib.h"
 
-int	xpipe(int pipefd[2])
+int	*xpipe(int *pipefd)
 {
-  if ((pipe(pipefd[2])) == -1)
+  if ((pipe(pipefd) == -1)
     {
       print_error("Cannot open pipe\n");
       return (-1);
