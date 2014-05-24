c321e09f5c444c5f7158ccd722483ef25c24f1ad
Serge <serge.heitzler@epitech.eu>
2014-05-24 11:02:36 +0200

    serge

--- execution/execution_next.c
+++ execution/execution_next.c
@@ -5,7 +5,7 @@
 ** Login   <heitzl_s@epitech.net>
 **
 ** Started on  Sat May 24 08:07:16 2014 heitzl_s
-** Last update Sat May 24 10:43:15 2014 heitzl_s
+** Last update Sat May 24 11:01:59 2014 heitzl_s
 */
 
 #include <unistd.h>
@@ -28,7 +28,8 @@ int     exec_redir(t_cmd *cmd, int i) // GERE QUE LES DROITES POUR L'INSTANT
   buffer = xcalloc(4096, sizeof(*buffer));
   if (cmd[i].type == 1 && cmd[i - 1].type == 1)
     {
-      if (strcmp(cmd[i - 1].token, ">") == 0 || strcmp(cmd[i - 1].token, ">>"))
+      if ((strcmp(cmd[i - 1].token, ">") == 0)
+	  || (strcmp(cmd[i - 1].token, ">>") == 0))
 	{
 	  read(cmd[i - 1].fdout, buffer, 4095);
 	  write(cmd[i].fdin, buffer, (strlen(buffer) + 1));
