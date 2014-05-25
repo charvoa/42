d530b8d8769d35fa5736db56f45e95b7dc48c284
charvo_a <nicolas.charvoz@epitech.eu>
2014-05-25 13:13:47 +0200

    lol

--- lexer/check2.c
+++ lexer/check2.c
@@ -5,54 +5,10 @@
 ** Login   <charvo_a@epitech.net>
 **
 ** Started on  Mon Apr 21 15:35:44 2014 Nicolas Charvoz
-** Last update Sun May 25 13:11:35 2014 Nicolas Charvoz
+** Last update Sun May 25 13:13:01 2014 Nicolas Charvoz
 */
 
 #include "lexer.h"
-
-int	check_error_none(t_token **token)
-{
-  t_token	*tok;
-
-  tok = *token;
-  while (tok != NULL)
-    {
-      if (tok->type == TOKEN_WORD && tok->next)
-	{
-	  tok = tok->next;
-	  if (tok->type == TOKEN_NONE)
-	    tok = tok->next;
-	}
-      if (tok->type > TOKEN_WORD && tok->next)
-	{
-	  tok = tok->next;
-	  if (tok->type == TOKEN_NONE && tok->next)
-	    {
-	      tok = tok->next;
-	      if (tok->type > TOKEN_WORD)
-		{
-		  fprintf(stderr, "NULL is not a command");
-		  return (-1);
-		}
-	    }
-	}
-      tok = tok->next;
-    }
-  return (0);
-}
-
-int	check_none(char *str, int i, t_token **token)
-{
-  if (str[i] == ' ')
-    {
-      while (str[i] == ' ')
-	{
-	  *token = insert(*token, TOKEN_NONE, " ", i);
-	  i++;
-	}
-    }
-  return (i);
-}
 
 int	check_and(char *str, int i, t_token **token)
 {
