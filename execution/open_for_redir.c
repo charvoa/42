2adb014ddda1c0c5cd7a7319465f1d646aa57098
garcia_t <garcia_t@epitech.eu>
2014-05-28 14:16:27 +0200

    redir

--- execution/open_for_redir.c
+++ execution/open_for_redir.c
@@ -5,7 +5,7 @@
 ** Login   <garcia_t@epitech.net>
 ** 
 ** Started on  Wed May 28 11:56:37 2014 garcia antoine
-** Last update Wed May 28 12:09:24 2014 garcia antoine
+** Last update Wed May 28 14:16:15 2014 garcia antoine
 */
 
 #include <stdlib.h>
@@ -18,17 +18,14 @@
 int	open_for_redir_right(char *name)
 {
   int	fd;
-
-  if (access(name, F_OK) == -1 || access(name, R_OK) == -1)
+  
+  if (access(name, R_OK) == -1)
     {
-      if (access(name, F_OK) == -1)
-	fprintf(stderr, "42sh: %s: no such file or directory.\n", name);
-      else
-	fprintf(stderr, "42sh: %s: Permission denied.\n", name);
+      fprintf(stderr, "42sh: %s: Permission denied.\n", name);
       return (-1);
     }
   fd = xopen(name, O_RDONLY | O_CREAT);
   if (fd == -1)
-    printf("42: open error;\n");
+    printf("42: open error.\n");
   return (fd);
 }
